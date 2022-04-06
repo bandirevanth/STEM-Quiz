b90cf4f031211fe0ecc5a8dd030346f759c3a7d3
Bandi Revanth <dhanush24082014@gmail.com>
2022-04-06 12:13:08 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,167 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does STEM stand for?",
+    answer: "Science, Technology, Engineering & Mathematics",
+    options: [
+      "Science, Technology, Engineering & Mathematics",
+      "silence, tao, enigma, and magic mushrooms",
+      "Starfish, Tongues, Ears, and More",
+      "Style, Technology, Engineering and Magic"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Animals that have a backbone fit into which category?",
+    answer: "Vertebrates",
+    options: [
+      "Viruses",
+      "Bacteria",
+      "Humans",
+      "Vertebrates"
+    ]
+  },
+    {
+    numb: 3,
+    question: "mL is the symbol for?",
+    answer: "milliliters",
+    options: [
+      "milliliters",
+      "Magnetism",
+      "Magnets",
+      "Magnesium"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Robert Fulton is known as the person who commercialized which method of transportation?",
+    answer: "Steamboat",
+    options: [
+      "Covered Wagon",
+      "Steamboat",
+      "Rocket",
+      "Submarine"
+    ]
+  },
+    {
+    numb: 5,
+    question: "On the periodic table of elements, what does 'O' stand for?",
+    answer: "Oxygen",
+    options: [
+      "Oxygen",
+      "Osmium",
+      "Oganesson",
+      "Ozone"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Geometry is the study of - ",
+    answer: "Shapes",
+    options: [
+      "The Human Form",
+      "Building design",
+      "Highway design",
+      "Shapes"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What do you call a mathematician who specializes in the field of geometry?",
+    answer: "Geometer",
+    options: [
+      "Geomagician",
+      "Geomatician",
+      "Geometer",
+      "Geoscientist"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which planet in our solar system is closest to the sun?",
+    answer: "Mercury",
+    options: [
+      "Venus",
+      "Mercury",
+      "Earth",
+      "Mars"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What is a very basic machine that consists of rope and a wheel?",
+    answer: "Pulley",
+    options: [
+      "Tripwire",
+      "Wheelie",
+      "Wheel-o",
+      "Pulley"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which is vital for keeping planets in their orbits?",
+    answer: "Gravity",
+    options: [
+      "Gravity",
+      "Yaw",
+      "Kryptonite",
+      "Hydroxide"
+    ]
+  },
+  {
+    numb: 11,
+    question: "What is the fastest land animal on Earth?",
+    answer: "Cheetah",
+    options: [
+      "Marlin",
+      "Carpenter Ant",
+      "Elephant",
+      "Cheetah"
+    ]
+  },
+  {
+    numb: 12,
+    question: "What is the fastest bird on Earth?",
+    answer: "Peregrine Falcon",
+    options: [
+      "Pigeon",
+      "Parrot",
+      "Peregrine Falcont",
+      "Sparrow"
+    ]
+  },
+  {
+    numb: 13,
+    question: "What tool do you use to measure temperature?",
+    answer: "Thermometer",
+    options: [
+      "Flask",
+      "Thermometer",
+      "Scale",
+      "Temper-o-meter"
+    ]
+  },
+  {
+    numb: 14,
+    question: "What's the name for a very common biological catalyst?",
+    answer: "Protien",
+    options: [
+      "Kerosene",
+      "Chemical",
+      "Protien",
+      "Lipid"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Who invented the phonograph?",
+    answer: "Thomas Edison",
+    options: [
+      "Bill Gates",
+      "John",
+      "Benjamin Franklin",
+      "Thomas Edison"
+    ]
+  },
+];
